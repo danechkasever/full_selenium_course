c56c8079955ade5e4378f25b0c86fa1516010af5
DaryaShumakova <dariashumakova@gmail.com>
2018-09-20 22:56:02 +0300

    3-я домашка

--- src/test/java/first/MyFirstTest.java
+++ src/test/java/first/MyFirstTest.java
@@ -2,7 +2,9 @@ package first;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,21 +15,38 @@ public class MyFirstTest {
     private WebDriver driver;
     private WebDriverWait wait;
     @Before
-    public void setUp() {
+    public  void setUp() {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 15);
     }
 
     @Test
-    public void firstTest() {
-        driver.get("https://www.yandex.ru/");
+        public void loginCorrect() throws InterruptedException {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("remember_me")).click();
+        driver.findElement(By.name("login")).click();
+        Thread.sleep(6000);
+        driver.findElement(By.className("fa-sign-out")).click();
+        driver.findElement(By.name("username")).isEnabled();
+
+    }
+
+    @Test
+    public void loginIncorrect() throws InterruptedException {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin1");
+        driver.findElement(By.name("login")).click();
+
 
     }
 
     @After
-    public  void finish() {
-        driver.quit();
-        driver = null;
+    public void finish() {
+      // driver.quit();
+      // driver = null;
     }
 
 }
